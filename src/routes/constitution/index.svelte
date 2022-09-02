5dc909a8ac95dff741ab9a69f8dbd4ae102fedf2
Joseph Houser <hjoseph@wustl.edu>
2022-09-02 11:36:46 -0500

    Update index.svelte
    added placeholder rules

--- src/routes/constitution/index.svelte
+++ src/routes/constitution/index.svelte
@@ -100,8 +100,11 @@
 </style>
 
 <div class="constitution">
-    <!-- <h1 class="noUnderscore">LEGENDS LEAGUE CONSTITUTION</h1>
+     <h1 class="noUnderscore">LEGENDS LEAGUE CONSTITUTION</h1>
+
+     <h2 class="noUnderscore">Coming Soon</h2>
     
+     <!--
     <h2 class="noUnderscore">TABLE OF CONTENTS</h2>
     
     <h3 class="noUnderscore clickable" on:click={() => goToSection(one)}>Section 1: Roster</h3>
